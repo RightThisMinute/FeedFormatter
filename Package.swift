50eb0bc3c03fe548a56383f0b57961bb565cf28e
Donovan Mueller <zotobi@gmail.com>
2016-11-01 10:29:29 -0700

    Add Zewo/HTTP dependency for LogMiddleware.

--- Package.swift
+++ Package.swift
@@ -7,6 +7,8 @@ let package = Package(
                majorVersion: 0, minor: 14),
       .Package(url: "https://github.com/Zewo/File.git",
                majorVersion: 0, minor: 14),
+      .Package(url: "https://github.com/Zewo/HTTP.git",
+               majorVersion: 0, minor: 14),
       .Package(url: "https://github.com/Zewo/HTTPClient.git",
                majorVersion: 0, minor: 14),
       .Package(url: "https://github.com/Zewo/HTTPServer.git",
