fbebe99ced848a3f505798dfac085a6f019da4a3
Donovan Mueller <zotobi@gmail.com>
2016-11-01 10:18:57 -0700

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,21 @@
+import PackageDescription
+
+let package = Package(
+    name: "MRSSFormatter",
+    dependencies: [
+      .Package(url: "https://github.com/Zewo/Axis.git",
+               majorVersion: 0, minor: 14),
+      .Package(url: "https://github.com/Zewo/File.git",
+               majorVersion: 0, minor: 14),
+      .Package(url: "https://github.com/Zewo/HTTPClient.git",
+               majorVersion: 0, minor: 14),
+      .Package(url: "https://github.com/Zewo/HTTPServer.git",
+               majorVersion: 0, minor: 14),
+      .Package(url: "https://github.com/Zewo/Mapper.git",
+               majorVersion: 0, minor: 14),
+      .Package(url: "https://github.com/Danappelxx/MuttonChop.git",
+               majorVersion: 0, minor: 2),
+      .Package(url: "https://github.com/behrang/YamlSwift.git",
+               majorVersion: 3, minor: 0),
+    ]
+)
