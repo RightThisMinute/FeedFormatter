940c70ddd45b51f1b0df96119e3e3cf32b79996e
Donovan Mueller <zotobi@gmail.com>
2016-11-10 15:39:15 -0700

    Rename Swift package to FeedFormatter.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MRSSFormatter",
+    name: "FeedFormatter",
     dependencies: [
       .Package(url: "https://github.com/Zewo/Axis.git",
                majorVersion: 0, minor: 14),
