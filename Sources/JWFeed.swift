7da34284d999ec28e72b6170e9bc21eb252e4d62
Donovan Mueller <zotobi@gmail.com>
2017-02-22 16:48:59 -0700

    Get larger version of JW thumbnails.

--- Sources/JWFeed.swift
+++ Sources/JWFeed.swift
@@ -19,11 +19,11 @@ struct JWFeed {
 }
 
 extension JWFeed : InMappable {
-	
+
 	enum MappingKeys : String, Mapper.IndexPathElement {
 		case kind, feedid, playlist, title
 	}
-	
+
 	init<Source : InMap>(mapper: InMapper<Source, MappingKeys>) throws {
 		id = try mapper.map(from: .feedid)
 		title = try mapper.map(from: .title)
@@ -48,12 +48,12 @@ struct JWItem {
 }
 
 extension JWItem : InMappable {
-	
+
 	enum MappingKeys : String, Mapper.IndexPathElement {
 		case mediaid, title, description, pubdate, sources, duration, image,
 		     tracks, tags, link, custom
 	}
-	
+
 	init<Source : InMap>(mapper: InMapper<Source, MappingKeys>) throws {
 		mediaID = try mapper.map(from: .mediaid)
 		title = try mapper.map(from: .title)
@@ -61,11 +61,24 @@ extension JWItem : InMappable {
 		pubdate = try mapper.map(from: .pubdate)
 		sources = try mapper.map(from: .sources)
 		duration = UInt(try mapper.map(from: .duration) as Int)
-		image = try mapper.map(from: .image)
 		tracks = try mapper.map(from: .tracks)
 		tags = (try mapper.map(from: .tags) as String).split(separator: ",")
 		link = try mapper.map(from: .link)
-		
+
+
+		/// Use the largest available version of the thumbnails.
+		/// See https://developer.jwplayer.com/jw-platform/reference/v1/urls/thumbs.html
+
+		let image: URL = try mapper.map(from: .image)
+		let pattern = try Regex("-[0-9]+(\\.jpg)$", options: .extended)
+		let bigImageURL = image.absoluteString.replace(pattern, with: ".jpg")
+
+		if let url = URL(string: bigImageURL) {
+			self.image = url
+		} else {
+			self.image = try mapper.map(from: .image)
+		}
+
 		if let dict: [String: Map] = try? mapper.unsafe_map(from: .custom) {
 			custom = dict.map{ (k, v) in return (k, (try? v.get()) ?? "") }
 		} else {
@@ -85,11 +98,11 @@ struct JWSource {
 }
 
 extension JWSource : InMappable {
-	
+
 	enum MappingKeys : String, Mapper.IndexPathElement {
 		case label, type, file, width, height, duration
 	}
-	
+
 	init<Source : InMap>(mapper: InMapper<Source, MappingKeys>) throws {
 		label    = try? mapper.map(from: .label)
 		type     = try  mapper.map(from: .type)
@@ -98,7 +111,7 @@ extension JWSource : InMappable {
 		height   = try? mapper.map(from: .height)
 		duration = try? mapper.map(from: .duration)
 	}
-	
+
 }
 
 
@@ -108,14 +121,14 @@ struct JWTrack {
 }
 
 extension JWTrack : InMappable {
-	
+
 	enum MappingKeys : String, Mapper.IndexPathElement {
 		case kind, file
 	}
-	
+
 	init<Source : InMap>(mapper: InMapper<Source, MappingKeys>) throws {
 		kind = try mapper.map(from: .kind)
 		file = try mapper.map(from: .file)
 	}
-	
+
 }
